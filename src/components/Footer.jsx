50be282b83b486ac64a6960d2bd34eb0587aa6b4
Reiine Iangar <reiineiangar@gmail.com>
2025-11-04 00:32:12 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
 function Footer() {
+    const [text, setText] = useState("");
+
+  useEffect(() => {
+        fetch("/example.txt")
+          .then((response) => response.text())
+          .then((data) => setText(data))
+          .catch((err) => console.error("Error loading text file:", err));
+      }, []);
+    
     return ( 
         <footer>
             <p>Copyright © 2023 Reiine Iangar. All Rights Reserved</p>
+            <p>Loaded from - {text} </p>
         </footer>
      );
 }
